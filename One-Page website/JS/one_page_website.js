b231aeb2f233adaf19893e9cf23f15f906a48cc3
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-10-08 02:24:01 -0400

    lightbox

---
+++ One-Page website/JS/one_page_website.js
@@ -0,0 +1,22 @@
+const lightbox = document.createElement("div")
+lightbox.id = "lightbox"
+document.body.appendChild(lightbox);
+
+const images = document.querySelectorAll("img")
+images.array.forEach(Image => {
+  Image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = images.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+  
+})
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})

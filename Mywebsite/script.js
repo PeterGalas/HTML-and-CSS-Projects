bb50ebeefcad4d93a8e4a08884a25230fb658fd1
PeterGalas <90492666+PeterGalas@users.noreply.github.com>
2021-11-19 16:41:32 -0500

    Personal website upload

---
+++ Mywebsite/script.js
@@ -0,0 +1,42 @@
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+	    document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'blue.css'
+	}
+
+	if(mode == 'green'){
+		document.getElementById('theme-style').href = 'green.css'
+	}
+
+	if(mode == 'navy'){
+		document.getElementById('theme-style').href = 'navy.css'
+	}
+
+	localStorage.setItem('theme', mode)
+}
+
+window.alert("Hello welcome to my personal webpage! Here you will find out all about me and what I do as a Software Engineer!")
+
+
